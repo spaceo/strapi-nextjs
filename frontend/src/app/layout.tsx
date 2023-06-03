d24397616d999c271527a0d8bc6049c3c8357aa1
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-06-04 00:44:52 +0200

    Change page background and fix style precedence

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
 import "@shopify/polaris/build/esm/styles.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-white`}>
+      <body className={`${inter.className}`}>
         <div className="m-20">{children}</div>
       </body>
     </html>
